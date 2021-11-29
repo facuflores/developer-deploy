46a8ed86897999790ef2f04f857de5f1778ef162
facuflores09 <facundo.flores@openix.com.ar>
2021-11-29 16:53:39 -0300

    first-commit: setup initial

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
+})
+
+app.get("/env", function (req, res) {
+  const value = process.env.MENSAJE | "MENSAJE POR DEFAULT";
+  res.send(value)
+})
+
+app.listen(3000, 
+	() => console.log("Server is running..."));
